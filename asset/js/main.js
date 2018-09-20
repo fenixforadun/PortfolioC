d01fa3b6bea00a376ac30b4529b7f620fe403141
fenix <fenixforadun@gmail.com>
2018-09-20 17:00:20 +0900

    50%

--- asset/js/main.js
+++ asset/js/main.js
@@ -60,7 +60,7 @@ $( document ).ready( function () {
 			}
 		}
 		return zero_prefix + num;
-	};
+	}
 
 	setInterval( nowClock, 1000 );
 
@@ -75,7 +75,7 @@ $( document ).ready( function () {
 		var section = articles.eq( indexs );
 		var offset = section.offset().top;
 
-		$( 'html, body' ).stop().animate( { scrollTop: offset }, 3000, 'swing');
+		$( 'html, body' ).stop().animate( {scrollTop: offset}, 3000, 'swing' );
 	} );
 
 	// to the top btn
@@ -95,13 +95,105 @@ $( document ).ready( function () {
 
 	// header_title fadeOut
 
-	$(window).scroll(function (  ) {
-		if ( $(this).scrollTop() > 2000 ) {
-			$('.header_title').fadeOut();
+	$( window ).on( 'scroll', function () {
+		var borderLine = $( '#section1' ).offset().top - 500;
+		var windowOffset = $( window ).scrollTop();
+		if ( windowOffset >= borderLine ) {
+			$( '.header_title' ).stop().fadeOut( 0 );
 		} else {
-			$('.header_title').fadeIn();
+			$( '.header_title' ).stop().fadeIn( 0 );
+		}
+	} );
+
+	// text effect
+
+	$( '.flow_text' ).html( function ( i, html ) {
+		var chars = $.trim( html ).split( "" );
+		return '<span>' + chars.join( '</span><span>' ) + '</span>';
+	} );
+
+	// desert
+
+	$( '#desert_wide_img' ).hover( function () {
+		$( '#desert_wrap' ).stop().animate( {opacity: '1'}, 500 );
+	}, function () {
+		$( '#desert_wrap' ).stop().animate( {opacity: '0'}, 500 );
+	} );
+
+	// door
+
+	$( '.door_bell > img' ).on( 'click', function () {
+		if ( $( '.panel' ).hasClass( 'open_panel' ) == false ) {
+			$( '.panel' ).addClass( 'open_panel' );
+			$( '.door_img_left' ).addClass( 'open_panel' );
+			$( '.door_img_right' ).addClass( 'open_panel' );
+		} else {
+			$('.panel').removeClass('open_panel');
+			$('.door_img_left').removeClass('open_panel');
+			$('.door_img_right').removeClass('open_panel');
+		}
+		$('.design_row_door').fadeOut(2000);
+	} );
+
+	/* $('a').on('click', function(){
+		$('.wrap, a').toggleClass('active');
+
+		return false;
+	}); */
+
+	// close door
+
+	$('.hyba_close').on('click', function (  ) {
+		$('.design_row_door').fadeIn(2000);
+		$('.panel').removeClass('open_panel');
+		$('.door_img_left').removeClass('open_panel');
+		$('.door_img_right').removeClass('open_panel');
+	});
+
+	$('#beWhite').click(function (  ) {
+		$('#section1').css("background", "#ffffff");
+	});
+
+	$('.circle').on('click', function (  ) {
+		var animationClass = $(this).data('animation');
+		var removeSec = $(this).data('remove');
+		if ( $(this) .hasClass(animationClass)) {
+			$(this).removeClass('animationClass');
+		} else {
+			$(this).addClass(animationClass);
+			if(typeof removeSec != 'undefined') {
+				var elapse = $(this);
+				setTimeout(function (  ) {
+					elapse.removeClass(animationClass);
+				}, removeSec);
+			}
 		}
 	});
 
+	$('.mainImg1').click(function (  ) {
+		$('.bouquet_wide').fadeIn(200);
+	});
 
+	$('.mainImg2').click(function (  ) {
+		$('.desert_wide').fadeIn(200);
+	});
+
+	$('.mainImg3').click(function (  ) {
+		$('.index2_wide').fadeIn(200);
+	});
+
+	$('.wide_img_close_btn').click(function (  ) {
+		$(this).parent().fadeOut(200);
+	});
+
+	$(window).on('scroll', function (  ) {
+		var toggleColorLine = $('#hamToggleLine').offset().top;
+		var windowOffset = $(window).scrollTop();
+		var hamburgers = $('.hamburger-inner');
+		if ( windowOffset >= toggleColorLine ) {
+			hamburgers.css('background-color', '#e6ae58');
+		} else {
+			hamburgers.css('background-color', '#fff');
+		}
+	});
 } );
--- asset/js/mainOPP.js
+++ asset/js/mainOPP.js
@@ -21,11 +21,11 @@ $( document ).ready( function () {
 		};
 
 		Index2.prototype.revealDataEach = function ( obj, n ) {
-			this.obj.children().eq( n ).stop().fadeIn( 2000 );
+			this.obj.children().eq( n ).stop().fadeIn( 500 );
 		};
 
 		Index2.prototype.hideDataEach = function ( obj, n ) {
-			this.obj.children().eq( n ).stop().fadeOut( 2000 );
+			this.obj.children().eq( n ).stop().fadeOut( 500 );
 		};
 
 		Index2.prototype.randColor = function ( obj ) {
@@ -43,13 +43,15 @@ $( document ).ready( function () {
 
 	}() );
 
-	var index2_ul = new Index2( $( '#index2_wrap' ) );
+	var index2_ul = new Index2( $( '.index_wide_wrap' ) );
 	index2_ul.initData( this );
 
 	$( '#index2' ).hover( function () {
+		$( '.index_wide_wrap' ).css( {'opacity': '1', 'visibility': 'visible'} );
 		index2_ul.revealData( this );
 	}, function () {
 		index2_ul.hideData( this );
+		$( '.index2_wide_wrap' ).css( {'opacity': '0', 'visibility': 'hidden'} );
 	} );
 
 	var hyba = new Index2( $( "#hyba" ) );
@@ -57,9 +59,7 @@ $( document ).ready( function () {
 
 	$( '#hyba > img' ).hover( function () {
 		hyba.hideDataEach( this, 1 );
-		hyba.revealDataEach( this, 2 );
 	}, function () {
-		hyba.hideDataEach( this, 2 );
 		hyba.revealDataEach( this, 1 );
 	} );
 
@@ -68,172 +68,5 @@ $( document ).ready( function () {
 	terminal.initData( this );
 	terminal.randColor( this );
 
-	// sakura
-
-	onload = function () {
-		initPetal();
-	};
-
-	function initPetal() {
-		window.PetalCanvas = new CanvasController( "petals" );
-		window.fallenPetals = 0;
-		var time = setInterval( petalAnimation, 50 );
-	}
-
-	function petalAnimation() {
-		if ( Math.random() > 0.25 && PetalCanvas.children.length < 200 )
-			addPetal( 1, 1, 1, PetalCanvas.width, 1 );
-
-		if ( fallenPetals > 50 ) {
-			var j = PetalCanvas.children.length;
-			for ( var i = 0; i < j; i++ ) {
-				var child = PetalCanvas.children[i];
-				if ( child.floatPetal ) child.floatPetal();
-			}
-			fallenPetals = 0;
-		}
-		PetalCanvas.rendering();
-	}
-
-	function CanvasController( id ) {
-		var canvas = document.getElementById( id );
-		canvas.width = 600;
-		canvas.height = 300;
-
-		this.canvas = canvas;
-		this.canvasContext = canvas.getContext( "2d" );
-		this.width = canvas.width;
-		this.height = canvas.height;
-		this.children = new Array();
-
-		this.rendering = function () {
-			this.clear();
-
-			var limit = this.children.length;
-			for ( var i = limit - 1; i >= 0; i-- ) {
-				this.canvasContext.save();
-				var child = this.children[i];
-
-				if ( child.draw( this.canvasContext ) ) {
-					child.callback();
-					this.removeChild( i );
-				}
-				this.canvasContext.restore();
-			}
-		};
-
-		this.clear = function () {
-			this.canvasContext.clearRect( 0, 0, this.width, this.height );
-		};
-		this.addChild = function ( child ) {
-			this.children.push( child );
-		};
-		this.removeChild = function ( num ) {
-			this.children.splice( num, 1 );
-		};
-	}
-
-	function random( n ) {
-		return Math.floor( Math.random() * n ) + 1;
-	}
-
-	function addPetal( num, x1, y1, x2, y2 ) {
-		for ( var i = 0; i < num; i++ ) {
-			var x_pos = Math.floor( Math.random() * ( x2 - x1 ) ) + x1;
-			var y_pos = Math.floor( Math.random() * ( y2 - y1 ) ) + y1;
-
-			PetalCanvas.addChild(
-					new Petals(
-							x_pos, y_pos, Math.random() + 0.5,
-							{x: random( 360 ), y: random( 360 ), z: random( 360 )},
-							{x: random( 10 ), y: random( 10 ), z: random( 10 )},
-							random( 5 )
-					)
-			);
-		}
-	}
-
-	function Petals( x, y, scale, direction, rotate, wind ) {
-		this.x_pos = x;
-		this.y_pos = y;
-		this.scale = scale;
-		this.direction = direction;
-		this.rotate = rotate;
-		this.wind = wind;
-		this.gr = 6;
-		this.length = 10;
-		this.phase = 0;
-
-		this.draw = function ( context ) {
-			context.beginPath();
-			context.translate( this.x_pos, this.y_pos );
-
-			context.rotate( this.direction.y / 180 * Math.PI );
-			context.scale( this.scale, this.scale );
-
-			var gradient = context.createRadialGradient( 0, 0, 0, 0, 0, this.length );
-			gradient.addColorStop( 0, "rgba(255, 250, 250, 1)" );
-			gradient.addColorStop( 0.6, "rgba(255, 230, 230, 1)" );
-			gradient.addColorStop( 1, "rgba(255, 150, 150, 0.4)" );
-			context.fillStyle = gradient;
-			context.shadowColor = "rgb(255, 255, 255)";
-			context.shadowBlur = 15;
-
-			var x_radi = Math.cos( this.direction.x * Math.PI / 180 );
-			var z_radi = Math.cos( this.direction.z * Math.PI / 180 );
-			context.moveTo( -6 * z_radi, -10 * x_radi );
-
-			context.bezierCurveTo( -10 * z_radi, 0 * x_radi, -5 * z_radi,
-					10 * x_radi, 0 * z_radi, 10 * x_radi );
-			context.bezierCurveTo( 5 * z_radi, 10 * x_radi, 10 * z_radi,
-					0 * x_radi, 6 * z_radi, -10 * x_radi );
-			context.bezierCurveTo( 0 * z_radi, -10 * x_radi, 0 * z_radi,
-					-7 * x_radi, 0 * z_radi, -5 * x_radi );
-			context.bezierCurveTo( 0 * z_radi, -7 * x_radi, 0 * z_radi,
-					-10 * x_radi, -6 * z_radi, -10 * x_radi );
-			context.fill();
-
-			return this.movePetals();
-		};
-		this.movePetals = function () {
-			var move_y;
-			if ( this.phase === 0 ) {
-				var ground = 0.8 + this.scale / 10;
-				if ( this.y_pos > PetalCanvas.height * ground ) {
-					this.gr = 0;
-					this.wind = 0;
-					this.rotate.x = 0;
-					this.rotate.y = 0;
-					this.rotate.z = 0;
-					this.phase = 1;
-					fallenPetals++;
-				}
-			} else if ( this.phase === 2 ) {
-				if ( this.gr > -3 ) this.gr += this.gr / 10;
-				move_y = this.gr * this.scale;
-			}
-			this.y_pos = this.y_pos + this.gr * this.scale;
-			this.x_pos = this.x_pos + this.wind;
-			this.direction.x += this.rotate.x;
-			this.direction.y += this.rotate.y;
-			this.direction.z += this.rotate.z;
-
-			if ( this.x_pos > PetalCanvas.width ) return true;
-			return this.y_pos > PetalCanvas.height ? true : false;
-		};
-
-		this.floatPetal = function () {
-			if ( this.phase === 3 ) {
-				this.gr = -Math.random();
-				this.wind = random( 15 ) + 5;
-				this.rotate.x = random( 10 ) + 15;
-				this.rotate.y = random( 10 ) + 15;
-				this.rotate.z = random( 10 ) + 15;
-				this.phase = 2;
-			}
-		};
-		this.callback = function () {
-		};
-	}
 
 } );
